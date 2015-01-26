597f50fcb53b6aa6ba313700db6dbe13b86c7edb
Paweł Krzaczkowski <pawel@freshmind.pl>
2015-01-26 17:21:10 +0100

    Client - not finished

--- src/Client.php
+++ src/Client.php
@@ -5,5 +5,29 @@ namespace WyszukiwarkaRegon;
 
 class Client
 {
+    /**
+     * Create new client
+     */
+    public function __construct()
+    {
 
+    }
+
+    /**
+     * Get captcha image
+     */
+    public function getCaptcha()
+    {
+
+    }
+
+    public function verifyCaptcha($params)
+    {
+
+    }
+
+    public function search($params)
+    {
+
+    }
 }
